3c4d34bacddbaf2264986df43ae02d8f873bd9d5
Pollyana Valverde <valverdepolly@gmail.com>
2026-04-05 18:44:20 -0300

    feat: create albumsList utility and update AlbumsBooksList component to use it

--- src/template/home-page/components/albums-books-list/albums-books-list.tsx
+++ src/template/home-page/components/albums-books-list/albums-books-list.tsx
@@ -2,38 +2,12 @@ import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Text } from "@/components/ui/text";
 
-const albumsCount: Album[] = [
-  {
-    name: "Fantasia",
-    booksCount: 2,
-    badge: "fantasy",
-  },
-  {
-    name: "Ficção",
-    booksCount: 2,
-    badge: "fiction",
-  },
-  {
-    name: "Romance",
-    booksCount: 2,
-    badge: "romance",
-  },
-  {
-    name: "Não-ficção",
-    booksCount: 2,
-    badge: "non-fiction",
-  },
-  {
-    name: "Ficção científica",
-    booksCount: 2,
-    badge: "sci-fi",
-  },
-];
+import { albumsList } from "@/utils";
 
 function AlbumsBooksList() {
   return (
     <Card className="p-0 gap-0">
-      {albumsCount.map((album) => (
+      {albumsList.map((album) => (
         <div
           key={album.badge}
           className="p-4 flex justify-between items-center not-first:border-t"
