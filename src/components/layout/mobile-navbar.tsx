d91225b47c5fa30a9c48ae218a1c6e9dc46557f4
Pollyana Valverde <valverdepolly@gmail.com>
2026-04-07 19:49:42 -0300

    feat: add Text component to MobileNavbar for improved link labeling and create global CSS module declaration

--- src/components/layout/mobile-navbar.tsx
+++ src/components/layout/mobile-navbar.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "../ui/nav-links";
+import { Text } from "../ui/text";
 
 import { NAVLINKS } from "@/utils/nav-links";
 
@@ -10,12 +11,15 @@ function MobileNavbar() {
     fixed bottom-0 left-0 right-0
     flex gap-2 items-center justify-around
     bg-gray-900 text-white rounded-xl
-    py-2 px-5 m-2 mx-auto w-[99%] 
+    py-2 px-1 my-1 mx-auto w-[99%] 
     `}
     >
       {NAVLINKS.map((link, index) => (
         <NavLink key={index} href={link.href} className="flex-col text-center">
           <link.icon className="mb-1 w-4 h-4" />
+          <Text variant="content-2">
+            {link.label === "Dashboard" ? "Home" : link.label.split(" ")[0]}
+          </Text>
         </NavLink>
       ))}
     </nav>
