c3fe877c87770106c9e351bd93ec07325af3559c
Pollyana Valverde <valverdepolly@gmail.com>
2026-08-10 11:39:30 -0300

    feat: add new private and public pages, update layout, and configure pnpm workspace
    - Introduced new pages for Home, Albums, Books Review, New Review, Sign In, and Sign Up.
    - Implemented a PrivateLayout component to structure private pages with a Navbar and Toaster.
    - Updated pnpm workspace configuration to allow builds for specific packages and excluded minimum release age.
    - Removed Navbar and MobileNavbar from the RootLayout to centralize layout management in PrivateLayout.

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,25 @@
+import "@/styles/globals.css";
+
+import { Toaster } from "sonner";
+import { Navbar, MobileNavbar } from "@/components/layout";
+
+export default function PrivateLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      <main
+        className={`
+          container mx-auto px-4 py-5 
+          mb-14 md:mb-0 md:mt-15`}
+      >
+        {children}
+      </main>
+      <MobileNavbar />
+      <Toaster position="top-right" />
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Roboto } from "next/font/google";
 import "@/styles/globals.css";
 
 import { Toaster } from "sonner";
-import { Navbar, MobileNavbar } from "@/components/layout";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -27,15 +26,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${roboto.variable} antialiased font-sans`}>
-        <Navbar />
-        <div
-          className={`
-          container mx-auto px-4 py-5 
-          mb-14 md:mb-0 md:mt-15`}
-        >
-          {children}
-        </div>
-        <MobileNavbar />
+        {children}
         <Toaster position="top-right" />
       </body>
     </html>
